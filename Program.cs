39b0063e2f46874123662685c641747ec4a8caa4
bozturkk <89193311+bozturkk@users.noreply.github.com>
2022-03-12 16:16:46 +0300

    Ders tamamlandı.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace While // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 den başlayarak console dan girilen sayıya kadar ortalama hesaplayıp console yazdıran progrma
+
+            Console.WriteLine("Lütfen bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi) //buraya yazılan ifade true olduğu sürece çalışacaktır.
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine("sayıların ortalaması" + toplam/sayi);
+
+
+            //a dan z ye kadar tüm harfleri console a yazdırma,
+
+            char karakter = 'a';
+            while (karakter <= 'z')
+            {
+                Console.Write(karakter + " ");
+                karakter ++;
+            }
+            Console.WriteLine(" ");
+            string[] arabalar = {"BMV", "Ford", "Toyota", "Nissan"};
+            foreach (var araba in arabalar) //tüm araba değerlerini teker teker gezerek yazdırır.
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
